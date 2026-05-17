dea9c8f2809f471cdde2e2296e303ce46da4b978
Danielandeche001 <daniel.andeche@student.moringaschool.com>
2026-05-17 05:33:42 +0300

    Implement plant shop functionality: fetch plants, add new plants, mark out of stock, and search

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+  const [outOfStock, setOutOfStock] = useState(new Set());
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((plants) => setPlants(plants));
+  }, []);
+
+  const handleAddPlant = (newPlant) => {
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPlant),
+    })
+      .then((response) => response.json())
+      .then((plant) => setPlants([...plants, plant]));
+  };
+
+  const handleToggleStock = (plantId) => {
+    const newOutOfStock = new Set(outOfStock);
+    if (newOutOfStock.has(plantId)) {
+      newOutOfStock.delete(plantId);
+    } else {
+      newOutOfStock.add(plantId);
+    }
+    setOutOfStock(newOutOfStock);
+  };
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search search={search} onSearchChange={setSearch} />
+      <PlantList
+        plants={filteredPlants}
+        outOfStock={outOfStock}
+        onToggleStock={handleToggleStock}
+      />
     </main>
   );
 }
